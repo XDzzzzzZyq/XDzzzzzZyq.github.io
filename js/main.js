344b9b94d3684c4b1fa075f28e6c1903d99240c1
Zyq-XDz <xiaodouzizyq@gmail.com>
2025-05-21 22:34:57 -0400

    5.21 split text

--- js/main.js
+++ js/main.js
@@ -5,8 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const arc = document.querySelector('.arc');
     const cir1 = document.querySelector('.cir1');
     const cir2 = document.querySelector('.cir2');
-    const title = document.querySelector('h1');
-
     gsap.to(sun, { rotation: 360, duration: 3, scrollTrigger: {
         trigger: sun,
         scrub: true,
@@ -24,8 +22,21 @@ document.addEventListener("DOMContentLoaded", function() {
         scale: 1, 
         duration: 1,
         transformOrigin: "50% 50%" });
-    gsap.fromTo(title, { y: -50, opacity: 0 }, { 
-        y: 0, 
-        opacity: 1, 
-        duration: 1 });
+
+    let split = new SplitText(".xdzzyq", { type: "chars" });
+    gsap.from(split.chars, {
+        duration: 0.5,
+        autoAlpha: 0,
+        yPercent: "random(-100, 100)",
+        stagger: {
+            amount: 0.5,
+            from: "random"
+        }
+    });
+
+    const arrow = document.querySelector('.arrow');
+
+    gsap.fromTo(arrow, { y: -30, opacity: 0 }, { 
+        y: -10, opacity: 1, 
+        duration: 0.5 });
 });
