14dac1e6086c6f2fb68b874787d0fe838f91604c
Zyq-XDz <xiaodouzizyq@gmail.com>
2025-05-21 21:11:44 -0400

    5.21 arrow

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(ScrollTrigger);
+
     const sun = document.querySelector('.sun');
     const arc = document.querySelector('.arc');
     const cir1 = document.querySelector('.cir1');
@@ -10,8 +12,20 @@ document.addEventListener("DOMContentLoaded", function() {
         scrub: true,
         markers: true
     } });
-    gsap.fromTo(arc, { opacity: 0 }, { opacity: 1, duration: 1 });
-    gsap.fromTo(cir1, { scale: 0 }, { scale: 1, duration: 1 });
-    gsap.fromTo(cir2, { scale: 0 }, { scale: 1, duration: 1 });
-    gsap.fromTo(title, { y: -50, opacity: 0 }, { y: 0, opacity: 1, duration: 1 });
+    gsap.fromTo(arc, { opacity: 0 }, { 
+        opacity: 1, 
+        duration: 1,
+        transformOrigin: "50% 50%" });
+    gsap.fromTo(cir1, { scale: 0 }, { 
+        scale: 1, 
+        duration: 1,
+        transformOrigin: "50% 50%" });
+    gsap.fromTo(cir2, { scale: 0 }, { 
+        scale: 1, 
+        duration: 1,
+        transformOrigin: "50% 50%" });
+    gsap.fromTo(title, { y: -50, opacity: 0 }, { 
+        y: 0, 
+        opacity: 1, 
+        duration: 1 });
 });
